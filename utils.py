53d3d1d34ae9d0a83d5ee918c486226dfbb4265b
Dimitrie David <david.dimitrie@gmail.com>
2023-06-08 18:29:16 +0300

    hotfix

--- utils.py
+++ utils.py
@@ -18,16 +18,28 @@ def upload_file_to_pod(api: client.CoreV1Api, name: str, file: UploadFile, desti
                   stdin=True, stdout=True, stderr=True, tty=False,
                   _preload_content=False)
 
+    commands = []
     with open(tar_file, "rb") as f:
-        commands = []
         while True:
             data = f.read(1024)
             if not data:
                 break
             commands.append(data)
-        resp.write_stdin(b"".join(commands))
-    print(resp.read_stdout())
-    print(resp.read_stderr())
+
+    
+    while resp.is_open():
+        resp.update(timeout=1)
+        if resp.peek_stdout():
+            print("STDOUT: %s" % resp.read_stdout())
+        if resp.peek_stderr():
+            print("STDERR: %s" % resp.read_stderr())
+        if commands:
+            c = commands.pop(0)
+            #print("Running command... %s\n" % c)
+            resp.write_stdin(c)
+        else:
+            break
+    resp.close()
 
     # remove tar file
     # resp = stream(api.connect_get_namespaced_pod_exec, name, "acadnet",
